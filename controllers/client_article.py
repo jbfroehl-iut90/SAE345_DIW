38182bf3ee0c8ad563b76052382fbe1bb16af77c
jb_frhl <146169903+jbfroehl-iut90@users.noreply.github.com>
2024-01-25 23:43:39 +0100

    updateuuuuuu

--- controllers/client_article.py
+++ controllers/client_article.py
@@ -14,12 +14,17 @@ def client_article_show():                                 # remplace client_ind
     mycursor = get_db().cursor()
     id_client = session['id_user']
 
-    sql = ''' select * from equipement '''
+    sql = ''' select * from EQUIPEMENT '''
     list_param = []
     condition_and = " where "
+    mycursor.execute(sql)
+    articles = mycursor.fetchall()
+
     # utilisation du filtre
-    sql3=''' select * from categorie_sport '''
-    articles =[]
+    sql3=''' select * from CATEGORIE_SPORT '''
+    # articles =[]
+    mycursor.execute(sql3)
+    categories = mycursor.fetchall()
 
     # pour le filtre
     types_article = []
@@ -27,8 +32,19 @@ def client_article_show():                                 # remplace client_ind
 
     articles_panier = []
 
+    # Nos types de sport
+
+    sqleu =  ''' select * from SPORT '''
+    mycursor.execute(sqleu)
+    sports = mycursor.fetchall()
+
+    sqleu2 = ''' select * from CATEGORIE_SPORT '''
+    mycursor.execute(sqleu2)
+    categories_sport = mycursor.fetchall()
+    print(categories_sport)
+
     if len(articles_panier) >= 1:
-        sql = '''  select * from equipement '''
+        sql = '''  select * from EQUIPEMENT'''
         prix_total = None
     else:
         prix_total = None
@@ -36,5 +52,5 @@ def client_article_show():                                 # remplace client_ind
                            , articles=articles
                            , articles_panier=articles_panier
                            #, prix_total=prix_total
-                           , items_filtre=types_article
+                           , items_filtre=types_article, sports=sports, categories=categories_sport
                            )
