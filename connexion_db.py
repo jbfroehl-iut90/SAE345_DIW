9b0cb4cc5b57129feaa7becf82775115053f8864
Y0WayzZ <91217688+Y0WayzZ@users.noreply.github.com>
2024-02-08 17:13:19 +0100

    kdo

--- connexion_db.py
+++ connexion_db.py
@@ -9,9 +9,9 @@ def get_db():
         db = g._database = pymysql.connect(
             host="localhost",
             # host="serveurmysql",
-            user="jbfroehl",
-            password="motdepassehehe",
-            database="BDD_jbfroehl",
+            user="jgenitri",
+            password="1511",
+            database="BDD_jgenitri_sae",
             charset='utf8mb4',
             cursorclass=pymysql.cursors.DictCursor
         )
