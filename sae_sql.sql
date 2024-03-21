dbbebb145f165861286aa4f093a846c233f99487
Y0WayzZ <91217688+Y0WayzZ@users.noreply.github.com>
2024-03-21 19:07:39 +0100

    sql modif

--- sae_sql.sql
+++ sae_sql.sql
@@ -1,8 +1,8 @@
-DROP TABLE IF EXISTS declinaison, ligne_commande, commande, etat, ligne_panier, note, equipement, categorie_sport, couleur, morphologie, marque, taille, utilisateur;  
+DROP TABLE IF EXISTS adresse, declinaison, ligne_commande, commande, etat, ligne_panier, note, commentaire, equipement, categorie_sport, couleur, morphologie, marque, taille, utilisateur;  
     
 
     
-CREATE TABLE UTILISATEUR (
+CREATE TABLE utilisateur (
   id_utilisateur INT AUTO_INCREMENT,
   login varchar(255),
   email varchar(255),
@@ -11,25 +11,25 @@ CREATE TABLE UTILISATEUR (
   nom varchar(255),
   est_actif tinyint(1),
   PRIMARY KEY (id_utilisateur)
-);  
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4;  
     
     
      
-INSERT INTO UTILISATEUR(id_utilisateur,login,email,password,role,nom,est_actif) VALUES
-    (NULL,'admin','admin@admin.fr',
+INSERT INTO utilisateur(id_utilisateur,login,email,password,role,nom,est_actif) VALUES
+(NULL,'admin','admin@admin.fr',
     'sha256$dPL3oH9ug1wjJqva$2b341da75a4257607c841eb0dbbacb76e780f4015f0499bb1a164de2a893fdbf',
     'ROLE_admin','admin','1'),
-    (2,'client','client@client.fr',
+(2,'client','client@client.fr',
     'sha256$1GAmexw1DkXqlTKK$31d359e9adeea1154f24491edaa55000ee248f290b49b7420ced542c1bf4cf7d',
     'ROLE_client','client','1'),
-    (3,'client2','client2@client2.fr',
+(3,'client2','client2@client2.fr',
     'sha256$MjhdGuDELhI82lKY$2161be4a68a9f236a27781a7f981a531d11fdc50e4112d912a7754de2dfa0422',
     'ROLE_client','client2','1');
     
     
 
      
-    CREATE TABLE categorie_sport(
+CREATE TABLE categorie_sport(
     id_categorie_sport INT AUTO_INCREMENT,
     libelle_categorie_sport VARCHAR(255),
     PRIMARY KEY(id_categorie_sport)
@@ -38,7 +38,7 @@ INSERT INTO UTILISATEUR(id_utilisateur,login,email,password,role,nom,est_actif) 
     
     
      
-    INSERT INTO categorie_sport (libelle_categorie_sport) VALUES
+INSERT INTO categorie_sport (libelle_categorie_sport) VALUES
         ('Sport de combat'),
         ('Sport collectif'),
         ('Sport de raquettes'),
@@ -52,8 +52,7 @@ INSERT INTO UTILISATEUR(id_utilisateur,login,email,password,role,nom,est_actif) 
     
 
 
-     
-    CREATE TABLE couleur(
+CREATE TABLE couleur(
     id_couleur INT AUTO_INCREMENT,
     libelle_couleur VARCHAR(255),
     couleur_anglais VARCHAR(255),
@@ -61,8 +60,7 @@ INSERT INTO UTILISATEUR(id_utilisateur,login,email,password,role,nom,est_actif) 
 );
      
     
-     
-    INSERT INTO couleur(libelle_couleur, couleur_anglais) VALUES
+INSERT INTO couleur(libelle_couleur, couleur_anglais) VALUES
     ('Rouge', 'red'),
     ('Bleu', 'blue'),
     ('Vert', 'green'),
@@ -78,16 +76,16 @@ INSERT INTO UTILISATEUR(id_utilisateur,login,email,password,role,nom,est_actif) 
          
     
 
-     
-    CREATE TABLE morphologie(
+      
+CREATE TABLE morphologie(
     id_morphologie INT AUTO_INCREMENT,
     libelle_morphologie VARCHAR(255),
     PRIMARY KEY(id_morphologie)
 );
      
     
-     
-    INSERT INTO morphologie (libelle_morphologie) VALUES
+      
+INSERT INTO morphologie (libelle_morphologie) VALUES
     ('Homme'),
     ('Femme'),
     ('Enfant'),
@@ -95,16 +93,16 @@ INSERT INTO UTILISATEUR(id_utilisateur,login,email,password,role,nom,est_actif) 
                  
     
 
-     
-    CREATE TABLE marque(
+      
+CREATE TABLE marque(
     id_marque INT AUTO_INCREMENT,
     libelle_marque VARCHAR(255),
     PRIMARY KEY(id_marque)
 );
          
     
-     
-    INSERT INTO marque (libelle_marque) VALUES
+      
+INSERT INTO marque (libelle_marque) VALUES
     ('Venum'),
     ('Adidas'),
     ('Nike'),
@@ -131,16 +129,16 @@ INSERT INTO UTILISATEUR(id_utilisateur,login,email,password,role,nom,est_actif) 
     
 
 
-     
-    CREATE TABLE taille(
+      
+CREATE TABLE taille(
     id_taille INT AUTO_INCREMENT,
     libelle_taille VARCHAR(255),
     PRIMARY KEY(id_taille)
 );
          
     
-    
-    INSERT INTO taille (libelle_taille) VALUES
+     
+INSERT INTO taille (libelle_taille) VALUES
     ('XS'),
     ('S'),
     ('M'),
@@ -176,8 +174,8 @@ INSERT INTO UTILISATEUR(id_utilisateur,login,email,password,role,nom,est_actif) 
           
     
      
-    
-    CREATE TABLE equipement(
+     
+CREATE TABLE equipement(
     id_equipement INT AUTO_INCREMENT,
     libelle_equipement VARCHAR(10000),
     prix_equipement INT,
@@ -195,8 +193,8 @@ INSERT INTO UTILISATEUR(id_utilisateur,login,email,password,role,nom,est_actif) 
     
 
     
-    
-    INSERT INTO equipement (libelle_equipement, prix_equipement, description_equipement, image_equipement, marque_equipement_id, sport_equipement_id, morphologie_equipement_id) VALUES
+     
+INSERT INTO equipement (libelle_equipement, prix_equipement, description_equipement, image_equipement, marque_equipement_id, sport_equipement_id, morphologie_equipement_id) VALUES
     ( 'Gants  de  MMA  Venum  Impact  2.0' , 80,  'Ces  gants  en  cuir  Skintex  sont  ergonomiques    confortables  et  de  haute  qualité.  La  mousse  triple  densite  permet  de  garder  votre  main  à  l  abris  des  chocs.  Forme  incurvée  permettant  au  gant  de  parfaitement  épouser  la  forme  de  votre  main.  Le  système  de  fermeture  fournit  un  meilleur  serrage  et  la  possibilité  de  fermer  avec  une  seule  main.' , 'gantsMMA.webp', 1, 1, 4),
     ( 'Gants  de  boxe  Venum  Elite' , 90,  'Gants  pour  Boxe  Anglaise    Kick  Boxing  et  Muay  Thai.  Doté  d  une  mousse  à  quadruple  densité    ces  gants  sont  designés  pour  absorber  au  mieux  les  impacts  lors  des  frappes.  Les  coutures  renforcées  et  les  panneaux  en  maille  combinés  à  leur  forme  ergonomique  vous  apporteront  un  ajustement  confortable  et  vous  aurez  l  impression  de  ne  faire  qu  un  avec  le  gant.' , 'gantsBoxe.webp', 1, 1, 4),
     ( 'Kimono  JJB  Venum  Elite  4.0' , 200,  'Ce  Kimono  est  fabriqué  en  coton  Pearl  Wave  450  g/m²  et  est  renforcé  sur  tous  les  points  de  tension  pour  une  plus  grande  durabilité.  Le  col  en  mousse  EVA  rigide  résistera  aux  prises  de  vos  adversaires  et  à  vos  ruptures  de  prise  les  plus  puissantes.  Confortable  et  robuste    il  est  conçu  pour  résister  à  l  usure  des  séances  d  entraînement  intenses.  Le  pantalon  est  fabriqué  en  tissu  Ripstop  285  g/m²  et  est  renforcé  aux  chevilles    aux  genoux  et  à  l  entrejambe.  Les  coutures  renforcées  aux  poignets  assurent  une  plus  grande  longévité' , 'kimonoJJB.webp', 1, 3, 1),
@@ -224,47 +222,67 @@ INSERT INTO UTILISATEUR(id_utilisateur,login,email,password,role,nom,est_actif) 
     
 CREATE TABLE note(
     id_note INT AUTO_INCREMENT,
-    note INT,
+    note DECIMAL(2,1),
     id_equipement INT,
+    utilisateur_id INT,
     PRIMARY KEY(id_note),
-    CONSTRAINT fk_note_equipement FOREIGN KEY(id_equipement) REFERENCES EQUIPEMENT(id_equipement));
+    CONSTRAINT fk_note_equipement FOREIGN KEY(id_equipement) REFERENCES equipement(id_equipement),
+    CONSTRAINT fk_note_utilisateur FOREIGN KEY(utilisateur_id) REFERENCES utilisateur(id_utilisateur));    
     
     
     
-    
-    INSERT INTO note (note, id_equipement) VALUES 
-    (4, 1),
-    (2, 1),
-    (5, 1),
-    (3, 1),
-    (4, 1),
-    (5, 1),
-    (4, 2),
-    (2, 2),
-    (5, 2),
-    (3, 2),
-    (4, 3),
-    (5, 3),
-    (4, 3),
-    (4, 3),
-    (1, 4),
-    (2, 4),
-    (3, 4),
-    (1, 4),
-    (4, 5),
-    (5, 5),
-    (2, 5),
-    (1, 5),
-    (4, 6),
-    (5, 6),
-    (2, 6),
-    (1, 6);
-    
+INSERT INTO note (note, id_equipement, utilisateur_id) VALUES 
+    (5.0, 1, 3),
+    (4.5, 1, 2),
+    (4.5, 1, 3),
+    (4.5, 2, 2),
+    (2.0, 3, 3),
+    (3.5, 3, 1),
+    (5.0, 4, 3),
+    (4.5, 4, 3),
+    (4.5, 4, 3),
+    (2.0, 4, 1),
+    (1.0, 5, 2),
+    (1.0, 5, 2),
+    (1.0, 5, 2),
+    (1.0, 5, 2),
+    (3.5, 6, 3),
+    (5.0, 6, 3),
+    (4.5, 7, 3),
+    (3.5, 8, 3),
+    (2.0, 9, 3),
+    (3.5, 8, 3),
+    (2.0, 9, 3),
+    (1.0, 10, 3),
+    (3.5, 9, 1),
+    (2.0, 10, 3),
+    (1.0, 10, 2),
+    (5.0, 11, 3),
+    (4.5, 11, 3),
+    (3.5, 11, 3),
+    (2.0, 11, 3),
+    (1.0, 11, 3),
+    (4.5, 12, 3),
+    (3.5, 13, 3),
+    (3.5, 13, 3),
+    (2.0, 15, 3),
+    (1.0, 16, 3),
+    (4.5, 17, 3),
+    (5.0, 17, 3),
+    (5.0, 17, 3),
+    (5.0, 17, 3),
+    (4.5, 18, 3),
+    (3.5, 19, 3),
+    (3.5, 20, 3),
+    (5.0, 1, 3),
+    (3.5, 19, 3),
+    (3.5, 20, 3)
+    ;
     
     
 
-    
-    CREATE TABLE etat(
+     
+CREATE TABLE etat(
     id_etat INT AUTO_INCREMENT,
     libelle_etat VARCHAR(255),
     PRIMARY KEY(id_etat)
@@ -272,16 +290,16 @@ CREATE TABLE note(
             
     
 
-    
-    INSERT INTO etat (libelle_etat) VALUES
+     
+INSERT INTO etat (libelle_etat) VALUES
     ('En cours de traitement'),
     ('Expédié'),
     ('Validé');
     
     
 
-    
-    CREATE TABLE commande(
+     
+CREATE TABLE commande(
     id_commande INT AUTO_INCREMENT,
     date_achat DATE,
     etat_id INT NOT NULL,
@@ -293,16 +311,16 @@ CREATE TABLE note(
             
     
 
-    
-    INSERT INTO commande (date_achat, etat_id, id_utilisateur) VALUES
+     
+INSERT INTO commande (date_achat, etat_id, id_utilisateur) VALUES
     ('2022-01-01', 1, 2),
     ('2022-01-01', 2, 2),
     ('2022-01-01', 3, 2);
     
     
 
-    
-    CREATE TABLE ligne_commande(
+     
+CREATE TABLE ligne_commande(
     commande_id INT,
     equipement_id INT,
     prix INT,
@@ -314,8 +332,8 @@ CREATE TABLE note(
             
     
 
-    
-    INSERT INTO ligne_commande (commande_id, equipement_id, prix, quantite) VALUES
+     
+INSERT INTO ligne_commande (commande_id, equipement_id, prix, quantite) VALUES
     (1, 1, 80, 2),
     (2, 2, 90, 1),
     (3, 3, 200, 1),
@@ -324,8 +342,8 @@ CREATE TABLE note(
     
     
 
-    
-    CREATE TABLE declinaison(
+     
+CREATE TABLE declinaison(
     id_declinaison INT AUTO_INCREMENT,
     couleur_declinaison INT,
     taille_declinaison INT,
@@ -339,8 +357,8 @@ CREATE TABLE note(
             
     
 
-    
-    INSERT INTO declinaison (couleur_declinaison, taille_declinaison, stock, id_equipement) VALUES
+     
+INSERT INTO declinaison (couleur_declinaison, taille_declinaison, stock, id_equipement) VALUES
     (1, 1, 10, 1),
     (2, 2, 10, 1),
     (3, 3, 10, 1),
@@ -404,7 +422,7 @@ CREATE TABLE note(
     
     
 
-    
+     
 CREATE TABLE ligne_panier(
     id_ligne_panier INT AUTO_INCREMENT,
     quantite INT,
@@ -415,6 +433,8 @@ CREATE TABLE ligne_panier(
     CONSTRAINT fk_ligne_panier_declinaison FOREIGN KEY (id_declinaison) REFERENCES declinaison(id_declinaison),
     CONSTRAINT fk_ligne_panier_utilisateur FOREIGN KEY (id_utilisateur) REFERENCES utilisateur(id_utilisateur)
 ); 
+            
+    
 
 CREATE TABLE commentaire(
     id_commentaire INT AUTO_INCREMENT,
@@ -427,17 +447,63 @@ CREATE TABLE commentaire(
     CONSTRAINT fk_commentaire_equipement FOREIGN KEY (equipement_id) REFERENCES equipement(id_equipement),
     CONSTRAINT fk_commentaire_utilisateur FOREIGN KEY (utilisateur_id) REFERENCES utilisateur(id_utilisateur)
     );
+    
+    
 
 INSERT INTO commentaire (commentaire, statut, date_publication, equipement_id, utilisateur_id) VALUES
-('Super produit', 1, '2022-11-11', 1, 3),
-('Bonne qualité', 1, '2022-11-11', 1, 2),
-('Je suis satisfait', 1, '2022-11-11', 2, 1),
-('A acheter', 0, '2022-11-11', 5, 1),
-('Super nickel', 1, '2022-11-11', 4, 2),
-('Parfait', 1, '2022-11-11', 1, 1),
-('Mauvaise qualité', 1, '2022-11-11', 3, 1),
+('Super produit (publié le 17/11)', 1, '2022-11-17', 1, 3),
+('Bonne qualité (publié le 15/11)', 1, '2022-11-15', 1, 2),
+('Je suis satisfait', 1, '2022-11-19', 2, 2),
+('A acheter', 0, '2022-11-12 10:15:15', 5, 2),
+('Super nickel', 1, '2022-10-11', 4, 2),
+('Mauvaise qualité', 0, '2022-11-11', 3, 3),
 ('Déjà cassé', 0, '2022-11-11', 1, 2),
-('Je recommande', 1, '2022-11-11', 1, 1),
-('A éviter', 1, '2022-11-11', 1, 3),
+('A éviter', 0, '2022-11-11', 1, 3),
 ('Je suis déçu', 1, '2022-11-11', 1, 2),
-('Je ne recommande pas', 0, '2022-11-11', 1, 1);
+('Je recommande', 1, '2022-11-11', 1, 2),
+('Je suis satisfait', 1, '2022-11-11', 5, 2),
+('Abimé', 0, '2022-11-11', 5, 2),
+('Je suis satisfait', 0, '2022-11-11', 5, 2),
+('Acheter pour mon fils', 1, '2022-11-11', 6, 3),
+('Bonne qualité', 1, '2022-11-11', 7, 3),
+('Je suis satisfait', 1, '2022-11-11', 7, 3),
+('Je recommande', 1, '2022-11-11', 8, 2),
+('Je suis satisfait', 0, '2022-11-11', 9, 2),
+('Je recommande', 0, '2022-11-11', 10, 2),
+('Je suis satisfait', 1, '2022-11-11', 11, 2),
+('Je recommande', 1, '2022-11-11', 14, 2),
+('Je suis satisfait', 1, '2022-11-11', 15, 2),
+('Je recommande', 0, '2022-11-11', 16, 2),
+('Je suis satisfait', 1, '2022-11-11', 17, 2),
+('Je recommande', 0, '2022-11-11', 18, 2),
+('Je suis satisfait', 1, '2022-11-11', 19, 2),
+('Je recommande', 1, '2022-11-11', 20, 2),
+('Je suis satisfait', 1, '2022-11-11', 19, 2),
+('Je recommande', 1, '2022-11-11', 20, 2);
+
+    
+
+     
+CREATE TABLE adresse(
+    id_adresse INT AUTO_INCREMENT,
+    adresse VARCHAR(255),
+    code_postal VARCHAR(255),
+    ville VARCHAR(255),
+    valide tinyint(1),
+    nom VARCHAR(255),
+    id_utilisateur INT,
+    PRIMARY KEY(id_adresse),
+    CONSTRAINT fk_adresse_utilisateur FOREIGN KEY (id_utilisateur) REFERENCES utilisateur(id_utilisateur)
+);
+            
+    
+
+     
+INSERT INTO adresse (adresse, code_postal, ville, valide,nom, id_utilisateur) VALUES
+    ('1 rue du groudron par terre', '75000', 'Paris',1,'Jean', 2),
+    ('2 rue des petits canards', '69000', 'Lyon',0,'Jeanne', 2),
+    ('3 rue du chocolat en poudre', '13000', 'Marseille',0,'Jean', 2),
+    ('4 chemin du pont', '31000', 'Toulouse',1,'Jeanne', 2),
+    ('5 rue du grand mechant loup', '33000', 'Bordeaux',1,'Jean', 2);
+    
+    
