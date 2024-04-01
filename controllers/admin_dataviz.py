2043309035eaa0820a563f2ae72b52113869ebe3
jb_frhl <146169903+jbfroehl-iut90@users.noreply.github.com>
2024-04-01 15:32:49 +0200

    update

--- controllers/admin_dataviz.py
+++ controllers/admin_dataviz.py
@@ -88,20 +88,20 @@ def show_type_article_stock():
 
 @admin_dataviz.route('/admin/dataviz/etat2')
 def show_dataviz_map():
-    # mycursor = get_db().cursor()
-    # sql = '''    '''
-    # mycursor.execute(sql)
-    # adresses = mycursor.fetchall()
+    mycursor = get_db().cursor()
+    sql = ''' SELECT COUNT(id_adresse) as nombre, code_postal FROM adresse GROUP BY code_postal; '''
+    mycursor.execute(sql)
+    adresses = mycursor.fetchall()
 
     #exemples de tableau "résultat" de la requête
-    adresses =  [{'dep': '25', 'nombre': 1}, {'dep': '83', 'nombre': 1}, {'dep': '90', 'nombre': 3}]
+    adresses =  [{'dep': '25', 'nombre': 1}, {'dep': '83', 'nombre': 1}, {'dep': '71', 'nombre': 3}]
 
     # recherche de la valeur maxi "nombre" dans les départements
     # maxAddress = 0
     # for element in adresses:
     #     if element['nbr_dept'] > maxAddress:
     #         maxAddress = element['nbr_dept']
-    # calcul d'un coefficient de 0 à 1 pour chaque département
+    # # calcul d'un coefficient de 0 à 1 pour chaque département
     # if maxAddress != 0:
     #     for element in adresses:
     #         indice = element['nbr_dept'] / maxAddress
